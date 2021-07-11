6bf4e4aa93ab0bc9b468a44d37d57341ffa07a93
Isaac <isaac.r.watts@gmail.com>
2021-07-10 19:16:02 -0600

    adding ngrx and began setting up store for testing/demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { ConnectFormDirective } from './connect-form.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    ConnectFormDirective
   ],
   imports: [
     BrowserModule,
     // other imports ...
     ReactiveFormsModule
+  ],
+  exports: [
+    ConnectFormDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormArray } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
+
 export class ProfileEditorComponent {
+  profileForm$: Observable<any> = this.store.select(state => state.profileForm)
   profileForm = this.fb.group({
     firstName: ['', Validators.required],
     lastName: [''],
@@ -22,12 +28,14 @@ export class ProfileEditorComponent {
       this.fb.control('')
     ])
   });
-
+  
   get aliases() {
     return this.profileForm.get('aliases') as FormArray;
   }
-
-  constructor(private fb: FormBuilder) { }
+  
+  constructor(private fb: FormBuilder, private store: Store<{profileForm: any}>){
+    this.profileForm$ = of(this.profileForm.value)
+  }
 
   updateProfile() {
     this.profileForm.patchValue({
@@ -45,5 +53,9 @@ export class ProfileEditorComponent {
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+    this.store.dispatch({
+      type: 'SUBMIT_FORM',
+      profileForm: this.profileForm.value
+    })
   }
 }
